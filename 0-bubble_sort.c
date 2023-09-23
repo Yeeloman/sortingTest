42278f8dbc999f258691611c94787dc887f5b8ea
Yandelf00 <zakaria.afyf00@gmail.com>
2023-09-23 14:33:06 +0100

    add

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,11 @@ void bubble_sort(int *array, size_t size)
 int temp;
 size_t i, j;
 
+
+if (!array || size == 0)
+{
+return;
+}
 for (i = 0; i < size - 1; i++)
 {
 for (j = 0; j < size - i - 1; j++)
