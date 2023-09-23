211ec998626d273fd135a91496de5b8199937698
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-09-23 17:09:34 +0100

    add task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,9 +1,29 @@
 #include "sort.h"
 
 /**
- *
- *
- *
+ *selection_sort - selection sort function
+ *@array: arrayof int
+ *@size: size of the array
  */
 
 void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j;
+	int min, tmp;
+
+	if (!array || size == 0)
+		return;
+	for (; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+			min = j;
+		}
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		print_array(array, size);
+	}
+}
