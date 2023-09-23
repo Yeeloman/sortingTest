3c0f83b891ef39ba6f5f3b25cd9e834223a51e6c
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-09-24 00:58:32 +0100

    add

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,17 +1,1 @@
-#include "sort.h"
 
-/**
- *
- *
- *
- *
- */
-
-int findPivot(int array, int start, int end) {}
-/**
- *quick_sort - function that uses quicl quick_sort algorithm
- *@array: array of numbers to be sorted
- *@size: size of the array
- */
-
-void quick_sort(int *array, size_t size) {}
