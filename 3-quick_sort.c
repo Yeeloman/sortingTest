0590949bb1e3906712cf83a378e0ba9d6e228b87
Mohamed Kaddouri <yeeloman@TheJester.eldergods.org>
2023-09-24 00:34:15 +0100

    add

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,9 +4,14 @@
  *
  *
  *
+ *
  */
 
-void quick_sort(int *array, size_t size) {
-  if (array[size] <= array[0])
-    return;
-}
+int findPivot(int array, int start, int end) {}
+/**
+ *quick_sort - function that uses quicl quick_sort algorithm
+ *@array: array of numbers to be sorted
+ *@size: size of the array
+ */
+
+void quick_sort(int *array, size_t size) {}
